c51ba8bb012d1941db018097771dc61b5ea2ad8c
nirajchand <149762449+nirajchand@users.noreply.github.com>
2025-01-07 12:44:02 +0545

    Adding database connection

---
+++ Database/db.js
@@ -0,0 +1,22 @@
+const {Sequelize} = require("sequelize");
+
+const sequelize = new Sequelize('Nepjobs_Databa','postgres','admin123',{
+    host: 'localhost',
+    dialect: 'postgres',
+    port: 5432,
+    logging: false,
+});
+
+async function testConnection(params) {
+    try{
+        await sequelize.authenticate();
+        console.log("DB connection successfull ...... ..... ... ")
+    }catch(error){
+        console.log("Unable to connect to database.......................", error);
+        
+    }
+}
+
+testConnection()
+
+module.exports = sequelize;
