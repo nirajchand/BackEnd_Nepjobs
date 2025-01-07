34862550c0153b7b8c248add6cf47f2b4c57870f
nirajchand <149762449+nirajchand@users.noreply.github.com>
2025-01-07 12:53:35 +0545

    modifying database connection

--- Database/db.js
+++ Database/db.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require("sequelize");
 
-const sequelize = new Sequelize('Nepjobs_Databa','postgres','admin123',{
+const sequelize = new Sequelize('Nepjobs_Database','postgres','admin123',{
     host: 'localhost',
     dialect: 'postgres',
     port: 5432,
