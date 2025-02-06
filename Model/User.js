dab2f28367cbdf807f27b7621202a6c5649fe8d4
nirajchand <149762449+nirajchand@users.noreply.github.com>
2025-02-06 21:14:59 +0545

    Adding model

--- Model/User.js
+++ Model/User.js
@@ -1,34 +1,42 @@
-
-const {DataTypes} = require('sequelize')
+const { DataTypes } = require("sequelize");
 
 const sequelize = require("../Database/db");
 
-const user = sequelize.define('usertable',{
-    id:{
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
+const user = sequelize.define("usertable", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement:true,
+    primaryKey: true,
+  },
+  fname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  gender: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  number: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  usertype: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
-    name:{
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    email:{
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-    },
-    password:{
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-    },
-    usertype:{
-        type: DataTypes.INTEGER,
-        unique: true,
-        allowNull: false,
-    }
-})
-
-module.exports = user
+module.exports = user;
